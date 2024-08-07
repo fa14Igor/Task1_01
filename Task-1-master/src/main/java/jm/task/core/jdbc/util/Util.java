2b3be0a6c41d4fc92ff74f9e12fb8cd107084bf1
fa14Igor <168740456+fa14Igor@users.noreply.github.com>
2024-08-07 14:04:33 +0300

    Add files via upload

---
+++ Task-1-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,10 @@
+package jm.task.core.jdbc.util;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+
+    // данные для подключения
+    private static final String URL = "jdbc:mysql://localhost:3306/your_database_name"; //  URL   базы   данных
+    private static final String USERNAME = "your_db_username";
+    private static final String PASSWORD = "your_db_password";
+}
